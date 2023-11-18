32138588982f53a73e97f1c76be7b7818a8d0b0b
sahoo <sahoosibaprasad2221@gmail.com>
2023-11-19 00:26:54 +0530

    Code changes

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,11 +4,16 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,5,6,isManual,"power stearing",5);
     }
 
+    public int getCurrentDirection(){
+        return 0;
+    }
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        /**
+
+        int newSpeed = rate ; //set the value of new speed by using currentSpeed and rate
+        /*
          * speed 0: gear 1
          * speed 1-50: gear 1
          * speed 51-100: gear 2
@@ -20,6 +25,32 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
+        }else{
+            if(newSpeed>=1 && newSpeed<=50){
+//                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(1);
+            }
+            else if(newSpeed>=51 && newSpeed<=100){
+//                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(2);
+            }
+            else if(newSpeed>=101 && newSpeed<=150){
+//                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(3);
+            }
+            else if(newSpeed>=151 && newSpeed<=200){
+//                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(4);
+            }
+            else if(newSpeed>=201 && newSpeed<=250){
+//                changeSpeed(newSpeed, getCurrentDirection());
+                changeGear(5);
+            }
+            else{
+                changeGear(6);
+            }
         }
         //for all other cases, change the gear accordingly
 
